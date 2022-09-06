afc26e88c6dba54b162cd78571108e35e8286de2
deltaTH <as933155@gmail.com>
2022-09-06 15:57:51 +0530

    affirmations app

---
+++ app/src/main/java/com/d3lta/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.d3lta.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
